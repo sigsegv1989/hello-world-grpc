15fc30d2e6762b10df541092f64dafee35f316ae
Abhijeet Rokade <sigsegv1989@gmail.com>
2023-08-23 14:08:00 +0000

    Enable Mutual Authentication using TLS/SSL certificates
    This commit implements mutual authentication between the gRPC client and server
    using TLS/SSL certificates. It ensures secure communication and authentication
    between the two components.
    
    Steps to generate Certificate and Key:
    1. Generate CA Certificate and Key:
       - Run the following commands to generate the CA certificate and key:
         ```
         openssl genpkey -algorithm RSA -out ca.key
         openssl req -x509 -new -key ca.key -out ca.crt -subj "/C=US/ST=California/L=San Francisco/O=My Organization/OU=IT Department/CN=My CA/emailAddress=ca@example.com"
         ```
    
    2. Generate Server Certificate and Key:
       - Run the following commands to generate the server certificate and key:
         ```
         openssl genpkey -algorithm RSA -out server.key
         openssl req -new -key server.key -out server.csr -subj "/CN=localhost" -addext "subjectAltName=DNS:localhost"
         openssl x509 -req -extfile <(printf "subjectAltName=DNS:localhost") -in server.csr -CA ../ca/ca.crt -CAkey ../ca/ca.key -CAcreateserial -out server.crt
         ```
    
    3. Generate Client Certificate and Key:
       - Run the following commands to generate the client certificate and key:
         ```
         openssl genpkey -algorithm RSA -out client.key
         openssl req -new -key client.key -out client.csr -subj "/CN=localhost" -addext "subjectAltName=DNS:localhost"
         openssl x509 -req -extfile <(printf "subjectAltName=DNS:localhost") -in client.csr -CA ../ca/ca.crt -CAkey ../ca/ca.key -CAcreateserial -out client.crt
         ```
    
    Generated Files Description:
    - `certs/ca/ca.crt`: CA certificate used to sign server and client certificates.
    - `certs/ca/ca.key`: Private key of the CA certificate.
    - `certs/server/server.crt`: Server's certificate signed by the CA.
    - `certs/server/server.key`: Private key of the server's certificate.
    - `certs/client/client.crt`: Client's certificate signed by the CA.
    - `certs/client/client.key`: Private key of the client's certificate.
    
    These certificates and keys are essential for enabling secure communication and authentication between the gRPC client and server.

--- client/client.go
+++ client/client.go
@@ -2,14 +2,46 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"flag"
+	"io/ioutil"
 	"log"
 
 	pb "github.com/sigsegv1989/hello-world-grpc/api/helloworld"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	// Define command-line flags with default values
+	clientCertPath := flag.String("client-cert", "certs/client/client.crt", "Path to client certificate file (PEM format)")
+	clientKeyPath := flag.String("client-key", "certs/client/client.key", "Path to client private key file (PEM format)")
+	serverCACertPath := flag.String("server-ca", "certs/ca/ca.crt", "Path to server CA certificate file (PEM format)")
+	flag.Parse()
+
+	// Load client certificate and key
+	clientCert, err := tls.LoadX509KeyPair(*clientCertPath, *clientKeyPath)
+	if err != nil {
+		log.Fatalf("Failed to load client certificate: %v", err)
+	}
+
+	// Load server's CA certificate
+	caCert, err := ioutil.ReadFile(*serverCACertPath)
+	if err != nil {
+		log.Fatalf("Failed to read CA certificate: %v", err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	// Create a credentials object using the client certificate and CA
+	creds := credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{clientCert},
+		RootCAs:      caCertPool,
+	})
+
+	// Set up gRPC connection with the credentials
+	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(creds))
 	if err != nil {
 		log.Fatalf("Failed to connect: %v", err)
 	}
@@ -17,7 +49,7 @@ func main() {
 	c := pb.NewGreeterClient(conn)
 
 	message := &pb.Message{
-		Name:     "Alice",
+		Name:     "sigsegv1989",
 		Greeting: "Hi there!",
 		Count:    0,
 	}
--- server/server.go
+++ server/server.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"flag"
+	"io/ioutil"
 	"log"
 	"net"
 
 	pb "github.com/sigsegv1989/hello-world-grpc/api/helloworld"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 type server struct {
@@ -22,11 +27,41 @@ func (s *server) SayHello(ctx context.Context, in *pb.Message) (*pb.Message, err
 }
 
 func main() {
+	// Define command-line flags with default values
+	serverCertPath := flag.String("server-cert", "certs/server/server.crt", "Path to server certificate file (PEM format)")
+	serverKeyPath := flag.String("server-key", "certs/server/server.key", "Path to server private key file (PEM format)")
+	clientCACertPath := flag.String("client-ca", "certs/ca/ca.crt", "Path to client CA certificate file (PEM format)")
+	flag.Parse()
+
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
+
+	// Load server certificate and key
+	serverCert, err := tls.LoadX509KeyPair(*serverCertPath, *serverKeyPath)
+	if err != nil {
+		log.Fatalf("Failed to load server certificate: %v", err)
+	}
+
+	// Load trusted client CA certificates
+	clientCACert, err := ioutil.ReadFile(*clientCACertPath)
+	if err != nil {
+		log.Fatalf("Failed to read client CA certificate: %v", err)
+	}
+	clientCACertPool := x509.NewCertPool()
+	clientCACertPool.AppendCertsFromPEM(clientCACert)
+
+	// Create a credentials object using the server certificate and client CA
+	creds := credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{serverCert},
+		ClientCAs:    clientCACertPool,
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+	})
+
+	// Set up gRPC server with the credentials
+	s := grpc.NewServer(grpc.Creds(creds))
+
 	pb.RegisterGreeterServer(s, &server{})
 	log.Println("Server listening on :50051")
 	if err := s.Serve(lis); err != nil {
